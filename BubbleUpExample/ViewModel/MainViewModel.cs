7b55338c2ee1474e5d65377ccaed552fc76bb03a
djangojazz <brettmorin2@gmail.com>
2017-08-25 08:47:48 -0700

    made changes to code

---
+++ BubbleUpExample/ViewModel/MainViewModel.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Windows;
+using System.Windows.Media;
+
+namespace BubbleUpExample
+{
+  public class MainViewModel : BaseViewModel
+  { 
+    public MainViewModel()
+    {
+
+
+      //InstanceConverter = new InstanceInSetToStringConverter();
+      //LocationCollection = new ObservableCollectionContentNotifying<DemandLocation>();
+      //ChartData = new ObservableCollectionContentNotifying<PlotTrend>();
+
+      //LocationCollection.ClearAndAddRange(Selects.GetDemandLocations().Take(5));
+      //var locs = new List<int> { 18, 55 };
+      //locs.ForEach(x => LocationCollection.Single(y => y.LocationID == x).IsUsed = true);
+      //UpdateHeader();
+      //SelectedItem = TrendChoices.FiscalPeriod;
+      //UpdateChartData();
+      //LocationCollection.OnCollectionItemChanged += UpdateHeader;
+    }
+    
+    
+    
+    private int count;
+    public int Count
+    {
+      get { return count; }
+      set
+      {
+        count = value;
+        OnPropertyChanged(nameof(Count));
+      }
+    }
+  }
+}
---
+++ BubbleUpExample/Views/CollectionChangedBubbleUpView.xaml.cs
@@ -0,0 +1,17 @@
+﻿using BubbleUpExample.ViewModel;
+using System.Windows.Controls;
+
+namespace BubbleUpExample
+{
+  /// <summary>
+  /// Interaction logic for CollectionChangedBubbleUpView.xaml
+  /// </summary>
+  public partial class CollectionChangedBubbleUpView : UserControl
+  {
+    public CollectionChangedBubbleUpView()
+    {
+      InitializeComponent();
+      DataContext = new CollectionChangedBubbleUpViewModel();
+    }
+  }
+}
